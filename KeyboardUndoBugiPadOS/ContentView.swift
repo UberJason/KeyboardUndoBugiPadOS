76ce2edf33fb8b4c63e64cb99de5fe05f002c910
Jason Ji <uberjason@gmail.com>
2020-02-19 22:13:29 -0500

    Update text in app

--- KeyboardUndoBugiPadOS/ContentView.swift
+++ KeyboardUndoBugiPadOS/ContentView.swift
@@ -40,13 +40,18 @@ struct ContentView: View {
         VStack(spacing: 16) {
             VStack(alignment: .leading) {
                 Text("Instructions").font(.system(.headline))
+                Group {
                 Text("1. Tap 'Register an Action' twice.")
                 Text("2. Observe the console printed that the action was registered twice, and the state is 2.")
                 Text("3. Tap 'Manual Undo (Not from Keyboard) once.")
                 Text("4. Observe the console printed that undo() was called and performUndo was called, and the state is now 1.")
                 Text("5. Tap 'Register an Action' twice more. The state should be 3.")
-                Text("6. Using an iPad with a keyboard attached, or iPad simulator with the setting 'Send Keyboard Shortcuts to Device', hit `CMD+Z`.")
-                Text("7. Observe the console printed that performUndo was called ") + Text("twice").fontWeight(.bold) + Text(", and the state is now 1. But it should only have been called once, and the state should be 2.")
+                Text("6. Using an iPad with a keyboard attached, or iPad simulator with the setting 'Send Keyboard Shortcuts to Device', hit `CMD+Z`.\n")
+                Text("On Xcode 11.3.1 and iPadOS 13.3:").italic()
+                Text("Observe the console printed that performUndo was called ") + Text("twice").fontWeight(.bold) + Text(", and the state is now 1. But it should only have been called once, and the state should be 2.\n")
+                Text("On Xcode 11.4 beta 2 and iPadOS 13.4 beta 2:").italic()
+                Text("Observe that the console printed that performUndo was called once, and the state is now 2. But any subsequent presses of CMD+Z do ") + Text("not").fontWeight(.bold) + Text(" call performUndo, and the state remains at 2.")
+                }
             }
             .padding([.leading, .trailing], 40)
             .padding(.bottom, 40)
